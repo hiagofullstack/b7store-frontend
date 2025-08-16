586858017df2f26626a6c20f37f3f4c99a03f39a
Hiago de Oliveira <hiagodeoliveiratv@gmail.com>
2025-08-16 16:54:17 -0300

    Antes de adicionar a store do carrinho zustand

---
+++ src/actions/get-cart-state.ts
@@ -0,0 +1,12 @@
+"use server"
+
+import { getServerCart } from "@/libs/server-cookies"
+
+
+export const getCartState = async  () => {
+
+    const cart = await getServerCart();
+    return {
+        cart
+    }
+}
